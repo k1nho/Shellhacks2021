d893f911120aed6f888a276e8092f48ec1503246
k1nho <kinhong99@gmail.com>
2021-09-25 17:41:30 -0400

    start landing page

---
+++ components/Home.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+    return (
+        <div className= "min-h-screen bg-gray-800 grid grid-cols-12 justify-items-center items-center">
+           <div className=" col-span-12 md:col-span-6 justify-items-center items-center"> Image</div>
+           <div className ="col-span-12 md:col-span-6 justify-items-center items-center"> Text</div> 
+        </div>
+    )
+}
