6e968b9e530ab3c5570d16000562ec241f14dc8e
k1nho <kinhong99@gmail.com>
2021-09-25 17:44:17 -0400

    stream component

--- components/Stream.tsx
+++ components/Stream.tsx
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import {Video} from "../components/Video"
 
 const Stream = () => {
+  const [videosIds, setVideosIds] = useState([]);
+
+  useEffect(()=> {
+    const hydrateIds = async() =>{
+    const getVideos = await fetch("/api/getvideos");
+    const data = await getVideos.json();
+    setVideosIds(data);
+  }
+
+    hydrateIds();
+  }, [])
+
   return (
-    <div className="max-w-6xl">
-      <div className="flex flex-col bg-gray-200 rounded-lg p-4 m-2">
-        <div className="h-40 bg-gray-400 rounded-lg"></div>
-        <div className="flex flex-col items-start mt-4">
-          <h4 className="text-xl font-semibold">Heading</h4>
-          <p className="text-sm">
-            Some text about the thing that goes over a few lines.
-          </p>
-          <a
-            className="p-2 leading-none rounded font-medium mt-3 bg-gray-400 text-xs uppercase"
-            href="#"
-          >
-            Click Here
-          </a>
-        </div>
-      </div>
+    
+    <div>
+      {videosIds.length !== 0 ?  videosIds.map((id: string) => (<div>
+        <Video publicId = {id}/>
+      </div>)) : <>no</>}
     </div>
   );
 };
