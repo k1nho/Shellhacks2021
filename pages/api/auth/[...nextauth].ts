c69fa88cd3433ea131070f4860915348ba8b4b12
Ehtesham Siddiqui <siddiquie.ehtesham@gmail.com>
2021-09-25 12:01:25 +0530

    Dashboard and Login Components

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,8 @@
 import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import Adapters from 'next-auth/adapters';
+import prisma from '../../../lib/prisma';
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
@@ -18,5 +20,6 @@ const options = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
   ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
   secret: process.env.SECRET,
 };
---
+++ pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container2 } from "../components/Container2";
+
+const dashboard = () => {
+  return (
+    <div>
+      <Container2 />
+    </div>
+  );
+};
+
+export default dashboard;
