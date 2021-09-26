935c805ae3291a7e83d8618f3bb5c0155ac5bbf0
Ehtesham Siddiqui <siddiquie.ehtesham@gmail.com>
2021-09-26 16:29:28 +0530

    Stream Video Styling and Thumbnail

--- components/Stream.tsx
+++ components/Stream.tsx
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from "react";
-import {Video} from "../components/Video"
+import { Video } from "../components/Video";
 
 const Stream = () => {
   const [videosIds, setVideosIds] = useState([]);
 
-  useEffect(()=> {
-    const hydrateIds = async() =>{
-    const getVideos = await fetch("/api/getvideos");
-    const data = await getVideos.json();
-    setVideosIds(data);
-  }
+  useEffect(() => {
+    const hydrateIds = async () => {
+      const getVideos = await fetch("/api/getvideos");
+      const data = await getVideos.json();
+      setVideosIds(data);
+    };
 
     hydrateIds();
-  }, [])
+  }, []);
 
   return (
-    
-    <div className= "grid grid-cols-12">
-      {videosIds.length !== 0 ?  videosIds.map((id: string) => (<div className="col-span-12 md:col-span-4">
-        <Video publicId = {id}/>
-      </div>)) : <>no</>}
+    <div className="grid grid-cols-12 gap-12 pt-12">
+      {videosIds.length !== 0 ? (
+        videosIds.map((id: string) => (
+          <div className="sm:col-span-12 md:col-span-4">
+            <Video publicId={id} />
+          </div>
+        ))
+      ) : (
+        <>no</>
+      )}
     </div>
   );
 };
--- components/Video.js
+++ components/Video.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Link from "next/dist/client/link";
+import VideoThumbnail from "react-video-thumbnail";
 
 export function Video({ publicId }) {
   const [videoPublicId, setPublicId] = useState(publicId);
@@ -10,21 +11,54 @@ export function Video({ publicId }) {
     return <></>;
   }
   return (
-    <div className="">
-
-        <video
-          className={`${videoPublicId.length === 0 ? "hidden" : "block m-4"}`}
-          controls
-          muted
-          src={`https://res.cloudinary.com/creatt/video/upload/w_400,h_200,q_auto/vc_auto/creatt/${videoPublicId}`}
-        ></video>
-            <Link href = {`/video/${videoPublicId}`}>
-        <button className=" m-4 px-2 py-2 rounded-md bg-yellow-500" >
-            
-            CreattWatch
-            </button>
-            </Link>
-    </div>
-
+    <Link href={`/video/${videoPublicId}`}>
+      <div className="">
+        <VideoThumbnail
+          videoUrl={`https://res.cloudinary.com/creatt/video/upload/w_400,h_200,q_auto/vc_auto/creatt/${videoPublicId}`}
+          className="min-w-full"
+        />
+        <div className="flex items-center mt-4">
+          <div className="rounded-full h-12 w-12 mr-4 flex items-center justify-center bg-green-500 text-green-300">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+              />
+            </svg>
+          </div>
+          <div>
+            <p className="text-xl font-semibold text-gray-50">
+              Awesome Stunts Video
+            </p>
+            <p className="text-gray-400">Ehtesham Siddiqui</p>
+          </div>
+        </div>
+        <div className="flex justify-end mt-3">
+          <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+            0.05 SATS/sec
+          </button>
+        </div>
+      </div>
+    </Link>
   );
+}
+
+//  <Link href = {`/video/${videoPublicId}`}></Link>
+{
+  /* <video
+  className={`${
+    videoPublicId.length === 0 ? "hidden" : "block m-4"
+  } w-full m-0 mb-4`}
+  controls
+  muted
+  src={`https://res.cloudinary.com/creatt/video/upload/w_400,h_200,q_auto/vc_auto/creatt/${videoPublicId}`}
+></video>; */
 }
