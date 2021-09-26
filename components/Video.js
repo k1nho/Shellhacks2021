0ad929ba6bc51b037e68310f69de08654c32f5c6
Ehtesham Siddiqui <siddiquie.ehtesham@gmail.com>
2021-09-26 18:32:39 +0530

    Feed Video Component Styling and Changes

--- components/Video.js
+++ components/Video.js
@@ -17,48 +17,12 @@ export function Video({ publicId }) {
           videoUrl={`https://res.cloudinary.com/creatt/video/upload/w_400,h_200,q_auto/vc_auto/creatt/${videoPublicId}`}
           className="min-w-full"
         />
-        <div className="flex items-center mt-4">
-          <div className="rounded-full h-12 w-12 mr-4 flex items-center justify-center bg-green-500 text-green-300">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-              />
-            </svg>
-          </div>
-          <div>
-            <p className="text-xl font-semibold text-gray-50">
-              Awesome Stunts Video
-            </p>
-            <p className="text-gray-400">Ehtesham Siddiqui</p>
-          </div>
-        </div>
-        <div className="flex justify-end mt-3">
+        <div className="mt-3">
           <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-            0.05 SATS/sec
+            Watch at 0.05 SATS/sec
           </button>
         </div>
       </div>
     </Link>
   );
-}
-
-//  <Link href = {`/video/${videoPublicId}`}></Link>
-{
-  /* <video
-  className={`${
-    videoPublicId.length === 0 ? "hidden" : "block m-4"
-  } w-full m-0 mb-4`}
-  controls
-  muted
-  src={`https://res.cloudinary.com/creatt/video/upload/w_400,h_200,q_auto/vc_auto/creatt/${videoPublicId}`}
-></video>; */
 }
--- pages/page1.tsx
+++ pages/page1.tsx
@@ -1,15 +1,20 @@
-import type { GetServerSideProps, NextPage,InferGetServerSidePropsType } from "next";
+import type {
+  GetServerSideProps,
+  NextPage,
+  InferGetServerSidePropsType,
+} from "next";
 import { getSession } from "next-auth/client";
 import Head from "next/head";
 // @ts-ignore
 import { useEffect, useState } from "react";
 import Login from "../components/Login";
-import {Upload} from "../components/Upload"
+import { Upload } from "../components/Upload";
 
-export default function page1({session}: InferGetServerSidePropsType<typeof getServerSideProps>){
-
-  if(!session){
-    return <Login/>
+export default function page1({
+  session,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!session) {
+    return <Login />;
   }
 
   return (
@@ -20,23 +25,19 @@ export default function page1({session}: InferGetServerSidePropsType<typeof getS
         <meta name=" author" content="default author" />
         <meta name="description" content=" page 1 description " />
       </Head>
-    <main className="flex h-screen justify-center items-center text-white ">
+      <main className="flex h-screen justify-center items-center text-white ">
         <div className="text-center bg-yellow-500 p-10 rounded-lg shadow-2xl">
-          <h1 className="text-3xl font-bold">
-            Creatt Video Upload
-          </h1>
+          <h1 className="text-3xl font-bold">Creatt Video Upload</h1>
           <Upload />
         </div>
-      </main> 
+      </main>
     </div>
   );
-};
+}
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   //Get user
   const session = await getSession(context);
 
-  return {props : {session}}
-
+  return { props: { session } };
 };
-
